c9f233c3cd14ce8af9b579f2fba58dc59f6719d4
snake <youdontseemehaha@gmail.com>
2017-06-12 14:37:48 -0700

    remove credentials for security

--- tenenceraspberrypi/credentials.py
+++ tenenceraspberrypi/credentials.py
@@ -6,13 +6,13 @@ with open('Tenence-688d118144e0.json') as data_file:
 # AWS Polly Credentials
 from boto3 import Session
 from botocore.exceptions import BotoCoreError, ClientError
-aws_access_key_id = 'AKIAJ5WWXSI4NCFPC7CA',
-aws_secret_access_key = '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',
+aws_access_key_id = '',
+aws_secret_access_key = '',
 region_name = 'us-east-2'
 
 session = Session(
-    aws_access_key_id='AKIAJ5WWXSI4NCFPC7CA',
-    aws_secret_access_key='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',
+    aws_access_key_id='',
+    aws_secret_access_key='',
     region_name='us-east-2'
 )
 polly = session.client('polly')
